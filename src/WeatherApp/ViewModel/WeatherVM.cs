720d5dabe226e87fc26f142567f4788f37cc3f5f
Błażej Domagała <petitoff@outlook.com>
2022-08-24 12:43:30 +0000

    public project Weather App

---
+++ src/WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Helpers;
+using WeatherApp.ViewModel.Commands;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChaned(nameof(Query));
+            }
+        }
+
+        public ObservableCollection<City> Cities { get; set; }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChaned(nameof(CurrentConditions));
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChaned(nameof(SelectedCity));
+                GetCurrentConditions();
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public WeatherVM()
+        { 
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+        private async void GetCurrentConditions()
+        {
+            Query = String.Empty;
+            Cities.Clear();
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChaned([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
